51b4aaffab59708010412379ca6ef090063b00e4
Hamunii <samuel.suni04@gmail.com>
2026-04-20 06:39:04 +0300

    commit verified snapshots

---
+++ tests/Snapshots/SnapshotTest._GameNameShortNoSpacesLowercase_mod.NoTutorialAndGitInclude.verified/NoTutorialAndGitIncludeTest/CHANGELOG.md
@@ -0,0 +1,3 @@
+﻿# Changelog
+
+TODO: You can follow this format for your changelog: <https://keepachangelog.com/en/1.1.0/>
---
+++ tests/Snapshots/SnapshotTest._GameNameShortNoSpacesLowercase_mod.NoTutorialAndGitInclude.verified/NoTutorialAndGitIncludeTest/src/NoTutorialAndGitIncludeTest/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace NoTutorialAndGitIncludeTest;
+
+/// <summary>
+/// The BepInEx plugin class of NoTutorialAndGitIncludeTest.
+/// </summary>
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
